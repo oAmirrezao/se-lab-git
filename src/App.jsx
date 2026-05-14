f038b7c2f1b1bb12d13e5f68a3e48699fb62514d
oamirrezao <inanlooamir9@gmail.com>
2026-05-14 22:34:57 +0330

    feat: integrate Contact section into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './pages/About';
 import Projects from './pages/Projects';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <About />
-            <Projects />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
 }
 export default App;
+
